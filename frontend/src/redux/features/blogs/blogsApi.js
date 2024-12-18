0449ad4ab44f135277bc6429b651b1361ebbaf73
karthiknotie <mkarthikeyan1808@gmail.com>
2024-12-18 20:34:10 +0530

    Update blogsApi.js

--- frontend/src/redux/features/blogs/blogsApi.js
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const blogApi = createApi({
   reducerPath: 'blogApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'http://prophecytechs.com/api',
   }),
 
   tagTypes: ['Blogs'],
