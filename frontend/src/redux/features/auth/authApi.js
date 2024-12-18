abedd4fa4f7253a9323da170153810f9f52d5fd0
karthiknotie <mkarthikeyan1808@gmail.com>
2024-12-18 20:33:36 +0530

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const authApi = createApi({
     reducerPath : 'authApi',
     baseQuery : fetchBaseQuery({
-        baseUrl : "http://localhost:5000/api/auth",
+        baseUrl : "http://prophecytechs.com/api/auth",
         credentials : "include",
     }),
     endpoints:(builder)=>({
